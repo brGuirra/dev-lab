4b9a6fb40a5149032095c5a10f184053c21b77c0
brGuirra <br.guirra@gmail.com>
2022-11-13 14:24:18 -0300

    test(beaches): ensure the server returns an error with status 500 when beach creation failed unexpectedly

--- __tests__/functional/beaches.test.ts
+++ __tests__/functional/beaches.test.ts
@@ -34,4 +34,20 @@ describe('Beaches functional tests', () => {
         'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
     });
   });
+
+  it('should return 500 when there is any error other than validation error', async () => {
+    jest.spyOn(Beach.prototype, 'save').mockRejectedValueOnce('fail to create a beach');
+
+    const beach = {
+      lat: Number(faker.address.latitude()),
+      lng: Number(faker.address.longitude()),
+      name: faker.address.cityName(),
+      position: BeachPosition.E,
+    };
+
+    const response = await global.testRequest.post('/beaches').send(beach);
+
+    expect(response.status).toBe(500);
+    expect(response.body).toEqual({ error: 'Internal Server Error' });
+  });
 });
