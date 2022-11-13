d06599d30cfb5b9f26034131962d9e29a449ea6e
brGuirra <br.guirra@gmail.com>
2022-11-13 14:15:16 -0300

    feat(beaches): ensure the server returns an error with status 422 when validation fails

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -2,16 +2,23 @@ import { Controller, Post } from '@overnightjs/core';
 import type { Request, Response } from 'express';
 
 import { Beach } from '@src/models/beach';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(request: Request<never, never, Beach, never>, response: Response): Promise<void> {
-    const { lat, lng, name, position } = request.body;
+    try {
+      const { lat, lng, name, position } = request.body;
 
-    const beach = new Beach({ lat, lng, name, position });
-    const result = await beach.save();
+      const beach = new Beach({ lat, lng, name, position });
+      const result = await beach.save();
 
-    response.status(201).send(result);
+      response.status(201).send(result);
+    } catch (error) {
+      if (error instanceof mongoose.Error.ValidationError) {
+        response.status(422).send({ error: error.message });
+      }
+    }
   }
 }
