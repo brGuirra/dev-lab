5b98f0571570aec7d9af18edeef5b508bee191eb
brGuirra <br.guirra@gmail.com>
2022-11-13 14:15:31 -0300

    test(beaches): ensure the server returns an error with status 422 when validation fails

--- __tests__/functional/beaches.test.ts
+++ __tests__/functional/beaches.test.ts
@@ -17,4 +17,21 @@ describe('Beaches functional tests', () => {
     expect(response.status).toBe(201);
     expect(response.body).toEqual(expect.objectContaining(beach));
   });
+
+  it('should throw an error when the data to create a beach is not valid', async () => {
+    const beach = {
+      lat: 'invalid_string',
+      lng: Number(faker.address.longitude()),
+      name: faker.address.cityName(),
+      position: BeachPosition.E,
+    };
+
+    const response = await global.testRequest.post('/beaches').send(beach);
+
+    expect(response.status).toBe(422);
+    expect(response.body).toEqual({
+      error:
+        'Beach validation failed: lat: Cast to Number failed for value "invalid_string" (type string) at path "lat"',
+    });
+  });
 });
